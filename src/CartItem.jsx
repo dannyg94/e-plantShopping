b19de053921650f6f19ea5772509fad5ca43089e
daniel <daniel.george94@gmail.com>
2025-10-25 07:11:16 -0400

    completed

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -7,29 +7,45 @@ const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
+
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
+  const calculateTotalAmount = (cart) => {
+    let total = 0;
+        cart.forEach((item) => {
+            total += parseFloat(item.cost.substring(1))* item.quantity;
+        })
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.onContinueShopping()
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
+    if (item.quantity > 1){
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }
+    else{
+        dispatch(removeItem(item));
+    }
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = 0
+    totalCost = parseFloat(item.cost.substring(1))* item.quantity;
+    return totalCost
   };
 
   return (
